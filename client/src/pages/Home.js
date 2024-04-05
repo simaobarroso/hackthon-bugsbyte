63220c3bc92fa11d311c34bb1e2a98ebbd519a63
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2024-04-06 00:20:57 +0100

    containers dos distritos a funcionar bem

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
-import './Home.css'; // Import CSS file
-import { ReactComponent as MapSvg } from '../assets/portugal_map.svg'; // Import SVG file
+import './Home.css'; // Importar arquivo CSS
+import { ReactComponent as MapSvg } from '../assets/portugal_map.svg'; // Importar arquivo SVG
 
 const Home = () => {
-    // Estado para controlar a exibição da mensagem
+    // Estado para controlar a exibição da mensagem e a classe de animação
     const [message, setMessage] = useState(null);
+    const [animationClass, setAnimationClass] = useState('');
 
     // Função para mostrar a mensagem
     const showMessage = (text) => {
         setMessage(text);
+        // Adiciona a classe de animação para deslocar o mapa para a esquerda
+        setAnimationClass('slide-left');
     };
 
     // Função para fechar a mensagem
     const closeMessage = () => {
         setMessage(null);
+        // Remove a classe de animação para deslocar o mapa para a direita
+        setAnimationClass('slide-right');
     };
 
     // Função para manipular o clique no contêiner SVG
@@ -23,35 +28,30 @@ const Home = () => {
             // Obtém o ID do path clicado
             const pathClass = event.target.getAttribute('class');
             // Aqui você pode associar o ID do path a uma mensagem específica
-            let messageText = '';
-            switch (pathClass) {
-                case 'z z31':
-                    messageText = 'Texto para Path 31';
-                    break;
-                case 'z z32':
-                    messageText = 'Texto para Path 32';
-                    break;
-                // Adicione mais casos conforme necessário para outros paths
-                default:
-                    messageText = 'Clique em um distrito para ver a mensagem' + pathClass;
-            }
+            let messageText = 'Clique em um distrito para ver a mensagem ' + pathClass;
             // Exibe a mensagem correspondente
             showMessage(messageText);
         }
     };
 
     return (
-        <div className="map-container" onClick={handleSvgClick}>
-            {/* SVG Component */}
-            <MapSvg className="map-svg" />
+        <div className="container">
+            {/* Mapa à esquerda */}
+            <div className={`map-container ${animationClass}`} onClick={handleSvgClick}>
+                <MapSvg className="map-svg" width="800px" height="600px" />
+            </div>
 
-            {/* Exibir a mensagem */}
-            {message && (
-                <div className="message-box">
-                    <p>{message}</p>
-                    <button onClick={closeMessage}>Fechar</button>
+            {/* Mensagem à direita */}
+            
+                {message && (
+                <div className="semi-container">
+                    <div className="message-box">
+                        <p>{message}</p>
+                        <button onClick={closeMessage}>Fechar</button>
+                    </div>
                 </div>
-            )}
+                )}
+            
         </div>
     );
 }
