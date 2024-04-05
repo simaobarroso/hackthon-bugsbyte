62a97cae6a21c12b127084e92abacbd23c20556d
EmanuelsGiT <emanuelmonteiro2002@gmail.com>
2024-04-05 22:22:12 +0100

    Fix

--- server/index.js
+++ server/index.js
@@ -11,13 +11,7 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
   
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
-
-
-
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
 
