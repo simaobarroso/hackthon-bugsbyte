a1c4c52cb5ce6eab15e07b3d52d09c82e3c02c00
simaobarroso <simao.barroso@outlook.pt>
2024-04-07 00:23:34 +0100

    Alteraçoes data

--- client/src/components/CulturalEvent/CulturalEvent.js
+++ client/src/components/CulturalEvent/CulturalEvent.js
@@ -41,10 +41,10 @@ const CulturalEvent = ({ event }) => {
 
     let d;
     if (data.dia < 10){
-        d = `0${data.dia}\\${month}\\${data.ano} (${data.diaDaSemana})`
+        d = `0${data.dia}\/${month}\/${data.ano} (${data.diaDaSemana})`
     }
     else {
-        d = `${data.dia}\\${month}\\${data.ano} (${data.diaDaSemana})`
+        d = `${data.dia}\/${month}\/${data.ano} (${data.diaDaSemana})`
     }
 
     const foto = "https://upload.wikimedia.org/wikipedia/commons/1/12/Museum_Benfica_dome.JPG"
@@ -61,7 +61,7 @@ const CulturalEvent = ({ event }) => {
               </div>
               <div className="info-icon">
                 <RiMapPin2Fill size={20} />
-                <span>{distrito}, {concelho}</span>
+                <span>{concelho},{distrito}</span>
               </div>
               <div className="info-icon">
                 <RiTicket2Fill size={20} />
